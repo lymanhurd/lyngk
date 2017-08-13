aa1a583026aa64875e52c81bdcb42219ad0c4dd7
Lyman Hurd <lyman.hurd@gmail.com>
2017-08-13 08:48:19 -0700

    Finalized code to determine winner.

--- lyngk.cpp
+++ lyngk.cpp
@@ -198,9 +198,18 @@ int Lyngk::GetWinner() {
   return -1;
 }
 
-// TODO(lyman): Implement.
+// Yes, this could be done in one pass (also the height check could be made more
+// efficient, but as this is only called at game end it was not worth optimizing.
 int Lyngk::StackCount(int player, int height) {
-  return 0;
+  int count = 0;
+  for (int row = 0; row < NUM_ROWS; row++) {
+    for (int col = 0; col < NUM_COLS; col++) {
+      if (StackHeight(row, col) == height && StackOwner(row, col) == player) {
+	count++;
+      }
+    }
+  }
+  return count;
 }
 
 bool Lyngk::CannotMove(int player) {
